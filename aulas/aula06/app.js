f3a92d579002cd3960fc50e517fa064596784f21
Raphael <raphaelferraz5056@gmail.com>
2025-09-16 21:57:28 -0300

    Adicionando testes aula 06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const tarefas = [];
+
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -13,5 +15,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+
+app.get('/tarefas', (req,res) => {
+    res.json(tarefas);
+});
+
+app.post('/tarefas', (req,res) => {
+    const novaTarefa = {
+        ...req.body,
+        id:tarefas.length + 1
+    };
+    tarefas.push(novaTarefa);
+    res.status(201).json(novaTarefa);
+});
+
+app.get('/tarefas/:id', (req,res) => {
+    const { id } = req.params;
+    const tarefaEncontrada = tarefas.find((item) => 
+        item.id === parseInt(id));
+    if(tarefaEncontrada){
+        console.log(tarefaEncontrada);
+        res.json(tarefaEncontrada);
+    } else{
+        res.status(404).json({msg:'Tarefa não encontrada'});
+    };
+});
 
 module.exports = app;
